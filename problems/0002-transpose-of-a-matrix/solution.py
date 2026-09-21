07747588acf18ef45d291072f41703dd6e1f831e
ID-JA <69154853+ID-JA@users.noreply.github.com>
2026-09-21 18:27:59 +0000

    Update: Transpose of a Matrix

--- problems/0002-transpose-of-a-matrix/solution.py
+++ problems/0002-transpose-of-a-matrix/solution.py
@@ -9,28 +9,6 @@ def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
         The transposed matrix of shape (n, m)
     """
     transpo_mat = []
-    # for irow, row in enumerate(a):
-    #     for icol, col in enumerate(row):
-    #         print("irow", irow)
-    #         print("icol", icol)
-    #         print(a[irow][icol])
-    #         break
-    
-    # irow = 0
-    # icol = 0 
-    # for row in a:
-    #     for col in row:
-    #         print(a[irow][icol])
-    #         break
-    #     irow+=1
-    #     if irow == len(a):
-    #         icol+=1
-
-    # for irow, row in enumerate(a):
-    #     for item_col in row:
-    #         transpo_mat.append([item_col])
-        
-    # print(transpo_mat)
     num_rows = len(a)
     num_cols = len(a[0])
 
