9c03e8cb4ba0dd7866dee65a6833ad535cd4914e
ID-JA <69154853+ID-JA@users.noreply.github.com>
2026-09-22 22:02:37 +0000

    Update: Reshape Matrix

--- problems/0003-reshape-matrix/solution.py
+++ problems/0003-reshape-matrix/solution.py
@@ -3,6 +3,7 @@ import numpy as np
 def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
 	#Write your code here and return a python list after reshaping by using numpy's tolist() method
 	
+	
 	reshaped_matrix = []
 	collen = len(a[0])
 	rowlen = len(a)
@@ -10,19 +11,15 @@ def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list
 		return []
 
 	
-	# for irow in range(rowlen):
-	# 	for icol in range(collen):
-	# 		for jcol in range(icol+2, collen, 2):
-	# 			print(f"{icol}:{jcol}")
-	# 			print(a[irow][icol:jcol])
 
-	for irow in range(rowlen):
-		for icol in range(0, collen, new_shape[1]):
-			new_row = a[irow][icol:icol + new_shape[1]]
-			reshaped_matrix.append(new_row)
-			# print(icol)
-			# print(a[irow][:icol])
+	# i did my own implmentation of reshaping matrix
+
+	# for irow in range(rowlen):
+	# 	for icol in range(0, collen, new_shape[1]):
+	# 		new_row = a[irow][icol:icol + new_shape[1]]
+	# 		reshaped_matrix.append(new_row)
 		
 
-	# print(a[0][0:[new_shape[1]]])
-	return reshaped_matrix
+	# or using numpy 
+	return np.array(a).reshape(new_shape).tolist()
+	# return reshaped_matrix
