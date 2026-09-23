651baa63e4480dc18b525c42487e869176e62e7b
ID-JA <69154853+ID-JA@users.noreply.github.com>
2026-09-23 22:07:33 +0000

    Solve: Calculate Mean by Row or Column

---
+++ problems/0004-calculate-mean-by-row-or-column/solution.py
@@ -0,0 +1,36 @@
+def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
+
+	means = []
+
+	# if mode == "row":
+	# 	for row in matrix:
+	# 		row_sum = 0
+
+	# 		for col in row:
+	# 			row_sum += col
+	# 		means.append(row_sum/len(row))
+			
+
+	# elif mode == "column":
+	rows = len(matrix)
+	cols = len(matrix[0])
+
+	if mode == "column":
+		for i in range(cols):
+			res_sum = 0
+			for j in range(rows):
+				res_sum += matrix[j][i]
+			means.append(res_sum/rows)
+	elif mode == "row":
+		for i in range(rows):
+			res_sum = 0
+			for j in range(cols):
+				res_sum += matrix[i][j]
+			means.append(res_sum/cols)
+				
+
+
+
+	
+
+	return means
